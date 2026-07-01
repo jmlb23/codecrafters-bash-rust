74679dbd4bcd8030c8a13064313662e190fa1050
jmlb23 <rmmenosrf@riseup.net>
2026-07-01 18:54:38 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io;
+use std::io::{Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    let mut cmd = String::new();
     print!("$ ");
     io::stdout().flush().unwrap();
+    let result = io::stdin().read_line(&mut cmd);
+    match result {
+        Ok(_size) => {
+            println!("{}: command not found", cmd.trim())
+        }
+        Err(er) => {
+            println!("Error reading from stdin {}", er.to_string())
+        }
+    }
 }
