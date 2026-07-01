a91d13caaecb1db54629511b01c2a1547447ae5c
jmlb23 <rmmenosrf@riseup.net>
2026-07-01 19:07:01 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
 use std::io;
-use std::io::{Write};
+use std::io::Write;
+use std::process::exit;
+
+fn parse_command(cmd: &str) -> i32 {
+    match cmd {
+        "exit" => 0,
+        _ => 1,
+    }
+}
 
 fn main() {
     let mut cmd = String::new();
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let result = io::stdin().read_line(&mut cmd);
-    match result {
-        Ok(_size) => {
-            println!("{}: command not found", cmd.trim())
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let result = io::stdin().read_line(&mut cmd);
+        match result {
+            Ok(_size) => {
+                if parse_command(cmd.trim()) == 0 {
+                    exit(0);
+                } else {
+                    println!("{}: command not found", cmd)
+                }
+            }
+            Err(er) => {
+                println!("Error reading from stdin {}", er.to_string())
+            }
         }
-        Err(er) => {
-            println!("Error reading from stdin {}", er.to_string())
-        }
+        cmd.clear();
     }
 }
