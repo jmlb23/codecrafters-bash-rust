074440649759530843f74ae0af4319e0d0082968
jmlb23 <rmmenosrf@riseup.net>
2026-07-03 21:59:26 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -157,7 +157,19 @@ fn main() {
                 Cmd::CdCmd(path) => {
                     let entry = Path::new(path.as_str());
                     if entry.exists() && entry.is_dir() {
-                        state.current_dir = entry.canonicalize().unwrap_or_default().to_str().unwrap_or_default().to_string()
+                        if entry.is_absolute() {
+                            state.current_dir = path
+                        } else {
+                            let current = state.current_dir;
+                            let new_path = current + "/" + path.as_str();
+                            state.current_dir =
+                                Path::new(new_path.as_str())
+                                    .canonicalize()
+                                    .unwrap_or_default()
+                                    .to_str()
+                                    .unwrap_or_default()
+                                    .to_string()
+                        }
                     } else {
                         println!("cd: {}: No such file or directory", path)
                     }
